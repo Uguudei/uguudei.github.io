9785fd273ff7e771dd58e7458ad29fcc50a070f7
Uguudei <1560166+Uguudei@users.noreply.github.com>
2021-11-30 18:20:26 +0800

    update

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,10 @@
 <script>
-	export let name;
+	let name = "Uguudei";
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>This is my portfolia demo</p>
 </main>
 
 <style>
